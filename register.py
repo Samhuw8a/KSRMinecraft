5a18ee87b70a7eaeffefe919755e693bb6feffa1
Samhuw8a <77882404+Samhuw8a@users.noreply.github.com>
2022-10-16 23:24:44 +0200

    Updates register

--- register.py
+++ register.py
@@ -26,25 +26,25 @@ else:
 if debug == True:
     print("register starting")
 
-username = str(config['credentials']['user'])
-password = str(config['credentials']['password'])
-server_ip = str(config['db']['server_ip'])
+#  username = str(config['credentials']['user'])
+#  password = str(config['credentials']['password'])
+#  server_ip = str(config['db']['server_ip'])
 mcpassword = str(config['credentials']['mcpassword'])
-engine = sqlalchemy.create_engine(f"mysql+pymysql://{username}:{password}@{server_ip}/Registration")
-query = f"INSERT INTO Registration.`user` (email, name, username, comment, `timestamp`) VALUES('{reg_mail}', '{reg_name}', '{reg_username}', NULL, current_timestamp());"
+#  engine = sqlalchemy.create_engine(f"mysql+pymysql://{username}:{password}@{server_ip}/Registration")
+#  query = f"INSERT INTO Registration.`user` (email, name, username, comment, `timestamp`) VALUES('{reg_mail}', '{reg_name}', '{reg_username}', NULL, current_timestamp());"
 
-with engine.connect() as con:
-    con.execute(query)
-
-def sql(sql_statement):
-    username = str(config['credentials']['user'])
-    password = str(config['credentials']['password']    )
-    server_ip = str(config['db']['server_ip'])
-    engine = sqlalchemy.create_engine(
-    f"mysql+pymysql://{username}:{password}@{server_ip}/Registration")
-    return pd.read_sql(str(sql_statement),
-                       con=engine
-                       )
+#  with engine.connect() as con:
+    #  con.execute(query)
+#
+#  def sql(sql_statement):
+#      username = str(config['credentials']['user'])
+#      password = str(config['credentials']['password']    )
+#      server_ip = str(config['db']['server_ip'])
+#      engine = sqlalchemy.create_engine(
+#      f"mysql+pymysql://{username}:{password}@{server_ip}/Registration")
+#      return pd.read_sql(str(sql_statement),
+#                         con=engine
+#                         )
 if debug == True:
     print(sql("SELECT * FROM user"))
 else:
