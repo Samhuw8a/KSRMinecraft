565f1a42f52bc374dcd1fdcd578b476309268351
Samhuw8a <77882404+Samhuw8a@users.noreply.github.com>
2022-10-16 23:15:32 +0200

    Updates

--- auth_token.py
+++ auth_token.py
@@ -67,7 +67,7 @@ def query():
     return sql(f'SELECT token FROM registration WHERE reg_mail = \'{reg_mail}\' ')
 
 def pull_token():
-    if query().empty:
+    if str(query())=="None":
         time.sleep(5)
         pull_token()
         ## exit strategy
